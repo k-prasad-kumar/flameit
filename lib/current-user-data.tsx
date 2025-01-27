916547b670c1af54e2f3ed50be2a6bc9103010aa
Prasad Kumar <kprasadkumar7@.com>
2025-01-27 23:56:53 +0530

    modified infinite scrolling for smoother experience

--- lib/current-user-data.tsx
+++ lib/current-user-data.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { cache } from "react";
 import { getUserByEmail } from "./actions/user.actions";
 
+// get loging user data
 export const getCurrentUser = cache(async () => {
   const session = await auth();
   const user = await getUserByEmail(session?.user?.email as string);
