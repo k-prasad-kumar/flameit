08e00c7dd36ea2920be22790342bb785e1e7ad7d
Prasad Kumar <kprasadkumar7@.com>
2025-01-28 20:31:02 +0530

    unistalled bcrypt and installed bcrypt js

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-// import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
