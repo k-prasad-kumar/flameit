3e84da8fc65c9f21d2abea56fd8b8043468c68c3
Prasad Kumar <kprasadkumar7@.com>
2025-01-29 02:09:22 +0530

    upadated post page

--- lib/current-user-data.tsx
+++ lib/current-user-data.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 import { cache } from "react";
 import { getUserByEmail } from "./actions/user.actions";
 
-// get loging user data
+// get loging user data with email
 export const getCurrentUser = cache(async () => {
   const session = await auth();
   const user = await getUserByEmail(session?.user?.email as string);
