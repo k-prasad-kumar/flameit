fa6110ad6de2808114e88328d980ee56e82df528
Prasad Kumar <kprasadkumar7@.com>
2025-02-15 19:29:01 +0530

    changed next-config-4

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  distDir: "build",
+  // distDir: "build",
   reactStrictMode: true,
   trailingSlash: true, // Important for Vercel
   compiler: {
